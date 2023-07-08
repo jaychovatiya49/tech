d8d152449d0d33d89468ed02e1aa5380e9053959
jay <jaychovatiya529@gmail.com>
2023-07-08 16:57:34 +0530

    add loops.py

---
+++ py_practice/loops.py
@@ -0,0 +1,18 @@
+#for i in [0,1,2,3,4,5]:
+#for i in range(6):
+#    print(i)
+
+##########################
+
+
+#names = ["divyesh", "rakesh", "kiran"] 
+
+#for name in names:
+#    print(names)
+
+##########################
+
+name = "jay"
+
+for character in name:
+    print(character)
